054a037072fcba3c015020a4fc757919e6a6b9dc
Пахолюк Олександр <destroyyy3333@gmail.com>
2026-09-09 12:52:28 +0300

    Add files via upload

---
+++ WelcomeScreen/script1.py
@@ -0,0 +1,28 @@
+print('Hello World')
+
+name = "Oleksandr"          # str — рядок
+age = 16              # int — ціле число
+height = 1.80          # float — дробове число
+is_student = True      # bool — логічний тип
+numbers = [1, 2, 3]    # list — список
+coordinates = (10, 20) # tuple — кортеж
+unique = {1, 2, 3}     # set — множина
+person = {"name": "Oleksandr", "age": 16}  # dict — словник
+
+a = 10
+b = 3
+
+print("Додавання:", a + b)
+print("Віднімання:", a - b)
+print("Множення:", a * b)
+print("Ділення:", a / b)
+print("Ділення націло:", a // b)
+print("Піднесення до степеня:", a ** b)
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
+print(numbers, type(numbers))
+print(coordinates, type(coordinates))
+print(unique, type(unique))
+print(person, type(person))
